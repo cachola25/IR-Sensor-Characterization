705d19c3f92d63cff5d3a39ffded7bc5c8a91c4d
cachola25 <cachola25@up.edu>
2025-04-18 00:14:58 -0700

    Turned overallModel into a class

---
+++ overallModels/overallModel.py
@@ -0,0 +1,107 @@
+import time
+import numpy as np
+import tensorflow as tf
+
+class overallModel:
+    
+    def __init__(self):
+        self.single_label_model = tf.keras.models.load_model('outcomePredictionModel3.keras')
+        self.regression_model = tf.keras.models.load_model('differentSizes2.keras')
+        self.data = np.loadtxt("test3.csv", delimiter=',')
+        self.sensor_data = self.data[:, :7]
+        self.max_value = np.max(self.sensor_data)
+        
+        
+    # Finds indices of the two highest peaks that are at least min_separation apart
+    def find_two_peaks(self,sensor_values, min_separation=1):
+        peaks = sensor_values.copy()
+        first_peak_idx = np.argmax(peaks)
+        first_peak_val = peaks[first_peak_idx]
+        
+        # Mask out a region around the first peak to find the second one
+        start = max(0, first_peak_idx - min_separation)
+        end = min(len(peaks), first_peak_idx + min_separation + 1)
+        peaks[start:end] = -np.inf  # Set nearby region to -inf so it's ignored
+        
+        second_peak_idx = np.argmax(peaks)
+        second_peak_val = peaks[second_peak_idx]
+
+        return first_peak_idx, second_peak_idx
+
+
+    # Masks sensor values to isolate peaks and sets other values mask_value
+    def create_masked_input(self,sensor_values, peak_idx, window_size=1, mask_value=3):
+
+        masked = np.full_like(sensor_values, fill_value=mask_value)
+        start = max(0, peak_idx - window_size)
+        end = min(len(sensor_values), peak_idx + window_size + 1)
+        masked[start:end] = sensor_values[start:end]
+        return masked
+    
+    def predict(self,ir_data):
+        copied_input = ir_data.copy()
+        predicted_probabilities = self.single_label_model.predict(ir_data)[0]
+        predicted_class = np.argmax(predicted_probabilities)
+        print(f"Predicted Number of Objects: {predicted_class}")
+        print(f"Predicted Probabilites: {predicted_probabilities}")
+        ret = []
+
+        if predicted_class != 0:
+            if predicted_class == 1:
+
+                predicted_output = self.regression_model.predict(ir_data[0])
+                predicted_distance, predicted_start_angle, predicted_end_angle = predicted_output[0]
+                predicted_start_angle_deg = np.degrees(predicted_start_angle)
+                predicted_end_angle_deg = np.degrees(predicted_end_angle)
+
+                if predicted_start_angle_deg > 180:
+                    predicted_start_angle_deg -= 180
+                if predicted_end_angle_deg > 180:
+                    predicted_end_angle_deg -= 180
+
+                # Print predictions
+                print(f"Predicted Distance: {predicted_distance}")
+                print(f"Start Angle: {predicted_start_angle_deg} degrees")
+                print(f"End Angle: {predicted_end_angle_deg} degrees")
+                ret.append((predicted_distance,predicted_start_angle_deg,predicted_end_angle_deg))
+
+            elif predicted_class == 2:
+                peak1, peak2 = self.find_two_peaks(copied_input[0])
+            
+                # first peak
+                input1 = self.create_masked_input(copied_input[0], peak1)
+                print(f"[Object 1] Masked IR values before normalization: {input1}")
+                input1_normalized = input1 / self.max_value
+                predicted_output = self.regression_model.predict(input1_normalized[np.newaxis, :])
+
+                predicted_distance, predicted_start_angle, predicted_end_angle = predicted_output[0]
+                predicted_start_angle_deg = np.degrees(predicted_start_angle)
+                predicted_end_angle_deg = np.degrees(predicted_end_angle)
+
+                if predicted_start_angle_deg > 180:
+                    predicted_start_angle_deg -= 180
+                if predicted_end_angle_deg > 180:
+                    predicted_end_angle_deg -= 180
+
+                # Print predictions
+                print(f"[Object 1] Predicted Distance: {predicted_distance:.2f} inches; Start Angle: {predicted_start_angle_deg:.2f} degrees; End Angle: {predicted_end_angle_deg:.2f} degrees")
+                ret.append((predicted_distance,predicted_start_angle_deg,predicted_end_angle_deg))
+
+                # second peak
+                input2 = self.create_masked_input(copied_input[0], peak2)
+                print(f"[Object 1] Masked IR values before normalization: {input2}")
+                input2_normalized = input2 / self.max_value
+                predicted_output = self.regression_model.predict(input2_normalized[np.newaxis, :])
+
+                predicted_distance, predicted_start_angle, predicted_end_angle = predicted_output[0]
+                predicted_start_angle_deg = np.degrees(predicted_start_angle)
+                predicted_end_angle_deg = np.degrees(predicted_end_angle)
+
+                if predicted_start_angle_deg > 180:
+                    predicted_start_angle_deg -= 180
+                if predicted_end_angle_deg > 180:
+                    predicted_end_angle_deg -= 180
+
+                # Print predictions
+                print(f"[Object 2] Predicted Distance: {predicted_distance:.2f} inches; Start Angle: {predicted_start_angle_deg:.2f} degrees; End Angle: {predicted_end_angle_deg:.2f} degrees")
+                ret.append((predicted_distance,predicted_start_angle_deg,predicted_end_angle_deg))
