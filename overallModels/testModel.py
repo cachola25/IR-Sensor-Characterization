34f0fc70f5ebb7e510cc652e5abd0d55dc3f390e
cachola25 <cachola25@up.edu>
2025-04-24 22:57:06 -0700

    overall done, code should fully cleaned now

--- overallModels/testModel.py
+++ overallModels/testModel.py
@@ -2,13 +2,12 @@ import numpy as np
 import tensorflow as tf
 from overallModel import overallModel
 
-# load single-label model to predict number of objects
-
 # Load and preprocess the data
 test_input = np.array([[1,28,97,75,16,101,11]], dtype=float)
 final_model = overallModel()
 prediction = final_model.predict(test_input)
 
 print(f"Model predicted: {len(prediction)} objects")
-for pred in prediction:
-    print(pred)
+for i, pred in enumerate(prediction):
+    predicted_distance, predicted_start_angle_deg, predicted_end_angle_deg = pred
+    print(f"[Object {i+1}] Predicted Distance: {predicted_distance:.2f} inches; Start Angle: {predicted_start_angle_deg:.2f} degrees; End Angle: {predicted_end_angle_deg:.2f} degrees")
